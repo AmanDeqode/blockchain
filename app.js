c6aa1804e230b71ea822de03fdcd573dc50efc93
Aman Chaudhary <achoudhary@deqode.com>
2021-11-08 20:21:15 +0530

    Create:Server

--- app.js
+++ app.js
@@ -1,35 +1,14 @@
-const bip39 = require('bip39');
-const hdkey = require('hdkey');
-const createHash = require('create-hash');
-const checkHash = require('bs58check');
+import express from 'express';
+import dotenv from 'dotenv';
 
-async function bitcoinHDwallet() {
-    const mnemonic = bip39.generateMnemonic(); // generate string
-    console.log(`mnemonic ${mnemonic}`);
-    const seed = await bip39.mnemonicToSeed(mnemonic); // create seed buffer
-    console.log(`seed: ${seed}`);
-    const root = hdkey.fromMasterSeed(seed);
-    console.log(`root ${JSON.stringify(root)}`);
-    const masterPrivateKey = root.privateKey.toString('hex');
+import routes from './routes/index';
+dotenv.config();
+const {PORT,HOST} = process.env;
 
-    console.log(`masterProivateKey ${masterPrivateKey}`)
+const app = express();
 
-    const addrssNode = root.derive("m/44'/60'/0'/0/0");
-    const publicKey = addrssNode._publicKey;
+app.use(routes);
 
-    console.log(publicKey);
-
-    const generateHash = createHash('sha256').update(publicKey).digest();
-    const rmHash = createHash('rmd160').update(generateHash).digest();
-
-    const generateBuffer = Buffer.allocUnsafe(21);
-
-    generateBuffer.writeUInt8(0x00,0);
-
-    rmHash.copy(generateBuffer,1);
-
-    const checkBase = checkHash.encode(generateBuffer);
-
-    console.log(`checkBase ${checkBase}`);
-}
-bitcoinHDwallet();
+app.listen(PORT, () => {
+    console.log(`Server is connected at http://${HOST}:${PORT}`);
+});
